8a7acc963577d8d768f3edbb3b75f4d43a153daf
Nam <namdocsat@gmail.com>
2022-04-14 09:58:45 -0400

    add routes handlers and middleware actions for routes

---
+++ routes/auth.routes.js
@@ -0,0 +1,15 @@
+//this file holds auth related routes.
+const express = require("express");
+
+const authController = require("../controllers/auth.controller");
+
+//create router object.
+const router = express.Router();
+
+//set up routes. All routes handlers which are defined with help from express router, take middleware functions as argument after the paths parameter
+router.get("/signup", authController.getSignup);
+
+router.get("/login", authController.getLogin);
+
+//module export tell nodejs which object, functions are defined in this file should be exposed to other files
+module.exports = router;
