3d9b67cf05f237c3ba303ea877f183b2415f2d38
Nam King <namdocsat@gmail.com>
2022-04-16 11:49:01 -0400

    add connection to mongoDB

---
+++ data/database.js
@@ -0,0 +1,23 @@
+const mongodb = require("mongodb");
+
+const Mongoclient = mongodb.MongoClient;
+
+let database;
+
+async function connectToDatabase() {
+    const client = await Mongoclient.connect('mongodb://localhost:27017');
+    database = client.db('online-shop');
+}
+
+
+function getDb() {
+    if (!database) {
+        throw new Error("you must connect first!");
+    }
+    return database;
+}
+
+module.exports = {
+    connectToDatabase: connectToDatabase,
+    getDb: getDb
+}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,6 +9,8 @@ const router = express.Router();
 //set up routes. All routes handlers which are defined with help from express router, take middleware functions as argument after the paths parameter
 router.get("/signup", authController.getSignup);
 
+router.post("/signup", authController.signup);
+
 router.get("/login", authController.getLogin);
 
 //module export tell nodejs which object, functions are defined in this file should be exposed to other files
