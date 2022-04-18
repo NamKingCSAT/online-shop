3513a3fb798827ce42107908e657ca52308a1728
Nam <namdocsat@gmail.com>
2022-04-18 14:13:23 -0400

    add serverside authentication. flash session to save user input in one input cycle

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/user.model");
 const authUtil = require("../util/authentication");
+const validation = require("../util/validation");
+const sessionFlash = require("../util/session-flash");
 
 function getSignup(req, res, next) {
   //render a template for the response
@@ -13,6 +15,40 @@ function getSignup(req, res, next) {
 }
 
 async function signup(req, res, next) {
+  const enteredData = {
+    email: req.body.email,
+    password: req.body.password,
+    fullname: req.body.fullname,
+    street: req.body.street,
+    postal: req.body.postal,
+    city: req.body.city,
+  };
+  if (
+    !validation.userDetailsAreValid(
+      req.body.email,
+      req.body.password,
+      req.body.fullname,
+      req.body.street,
+      req.body.postal,
+      req.body.city
+    ) ||
+    !validation.emailIsConfirmed(req.body.email, req.body["confirm-email"])
+  ) {
+    sessionFlash.flashDataToSession(
+      req,
+      {
+        errorMessage:
+          "Please check your input. Password must be at least 6 character long, postal code must be 5 characters long. ",
+        // the enteredData object bellow will be spread into this object here by spread operator
+        ...enteredData,
+      },
+      function () {
+        res.redirect("/signup");
+      }
+    );
+    res.redirect("/signup");
+    return;
+  }
   const user = new User(
     req.body.email,
     req.body.password,
@@ -23,6 +59,21 @@ async function signup(req, res, next) {
   );
 
   try {
+    const existsAlready = await user.existsAlready();
+    if (existsAlready) {
+      sessionFlash.flashDataToSession(
+        req,
+        {
+          errorMessage: "User exists already! Try logging in instead!",
+          ...enteredData,
+        },
+        function () {
+          res.redirect("/signup");
+        }
+      );
+      res.redirect("/signup");
+      return;
+    }
     await user.signup();
   } catch (error) {
     next(error);
@@ -46,7 +97,17 @@ async function login(req, res, next) {
     return;
   }
 
+  const sessionErrorData = {
+    errorMessage:
+      "Invalid credentials - please double-check your email and password",
+    email: user.email,
+    password: user.password,
+  };
+
   if (!existingUser) {
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect("/login");
+    });
     res.redirect("/login");
     return;
   }
@@ -56,7 +117,9 @@ async function login(req, res, next) {
   );
 
   if (!passwordIsCorrect) {
-    res.redirect("/login");
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect("/login");
+    });
     return;
   }
 
--- models/user.model.js
+++ models/user.model.js
@@ -1,36 +1,44 @@
-const db = require('../data/database')
-const bcrypt = require("bcryptjs")
+const db = require("../data/database");
+const bcrypt = require("bcryptjs");
 
 class User {
-    constructor(email, password, fullname, street, postal, city) {
-        this.email = email;
-        this.password = password;
-        this.name = fullname;
-        this.address = {
-            street,
-            postal,
-            city
-        }
-    }
+  constructor(email, password, fullname, street, postal, city) {
+    this.email = email;
+    this.password = password;
+    this.name = fullname;
+    this.address = {
+      street,
+      postal,
+      city,
+    };
+  }
 
-    async signup() {
-        const hashedPassoword = await bcrypt.hash(this.password, 12);
+  async signup() {
+    const hashedPassoword = await bcrypt.hash(this.password, 12);
 
-        await db.getDb().collection("users").insertOne({
-            email: this.email,
-            password: hashedPassoword,
-            name: this.name,
-            address: this.address
-        })
-    }
+    await db.getDb().collection("users").insertOne({
+      email: this.email,
+      password: hashedPassoword,
+      name: this.name,
+      address: this.address,
+    });
+  }
 
-    getUserWithSameEmail() {
-        return db.getDb().collection("users").findOne({ email: this.email });
-    }
+  getUserWithSameEmail() {
+    return db.getDb().collection("users").findOne({ email: this.email });
+  }
 
-    hasMatchingPassword(hashedPassoword) {
-        return bcrypt.compare(this.password, hashedPassoword)
+  async existsAlready() {
+    const existingUser = await this.getUserWithSameEmail();
+    if (existingUser) {
+      return true;
     }
+    return false;
+  }
+
+  hasMatchingPassword(hashedPassoword) {
+    return bcrypt.compare(this.password, hashedPassoword);
+  }
 }
 
 module.exports = User;
