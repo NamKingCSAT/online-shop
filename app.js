a6f0923d9f6c32ef470d596207f5344de3bd1346
Nam <namdocsat@gmail.com>
2022-04-22 14:48:14 -0400

    add product admin page form

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const path = require("path");
 
 const express = require("express");
-const csrf = require('csurf');
+const csrf = require("csurf");
 const expressSession = require("express-session");
 //where to store session
 const createSessionConfig = require("./config/session");
 //establish database connection before the web server
 const db = require("./data/database");
-const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
 //add error handling middleware
 const errorHandlerMiddleware = require("./middlewares/error-handler");
 const checkAuthStatusMiddleware = require("./middlewares/check-auth");
 const authRoutes = require("./routes/auth.routes");
-const productRoutes = require('./routes/products.routes');
-const baseRoutes = require('./routes/base.routes');
+const productRoutes = require("./routes/products.routes");
+const baseRoutes = require("./routes/base.routes");
+const adminRoutes = require("./routes/admin.routes");
 
 const app = express();
 
@@ -36,7 +37,7 @@ const sessionConfig = createSessionConfig();
 
 app.use(expressSession(sessionConfig));
 //to protect against csrf attack, the position is not important but have to be before the request hit your route
-app.use(csrf())
+app.use(csrf());
 
 app.use(addCsrfTokenMiddleware);
 app.use(checkAuthStatusMiddleware);
@@ -45,14 +46,16 @@ app.use(checkAuthStatusMiddleware);
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productRoutes);
+//only request that starts with /admin will make into this route configuration
+app.use("/admin", adminRoutes);
 
 app.use(errorHandlerMiddleware);
 
 db.connectToDatabase()
-    .then(function () {
-        app.listen(3000);
-    })
-    .catch(function (error) {
-        console.log("Failed to connect to the database!")
-        console.log(error);
-    });
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log("Failed to connect to the database!");
+    console.log(error);
+  });
